a14e50c97fd97a2586d43e61d5f86da7a534d0ed
Oscar Eduardo Anacona <oscared26@gmail.com>
2023-07-24 22:07:17 -0300

    Leitura e escrita de duas variáveis

---
+++ 2023_I/EADII/Aula01/Exemplo01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A,B,C;
+
+    printf("Ingresse o numero A=> \n");
+    scanf("%d",&A);
+
+    printf("Ingresse o numero B=> \n");
+    scanf("%d",&B);
+
+    C=A+B;
+
+    printf("O resultado de C=> \n%d",C);
+    return 0;
+}
