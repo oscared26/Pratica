d951257ee9f5888a83ffe2e4106ae5269e89ad97
Oscar Eduardo Anacona <oscared26@gmail.com>
2023-07-24 22:21:57 -0300

    Exemplo de somatorio usando WHILE

---
+++ 2023_I/EADII/Aula01/Exemplo03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sum;
+    sum=0;
+
+    while(sum<5){
+        sum=sum+1;
+    }
+
+    printf("Valor do somatorio=> \n %d", sum);
+
+    return 0;
+}
