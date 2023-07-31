1e2d1f190440819010265f8436f77f1c2993d5fa
Oscar Eduardo Anacona <oscared26@gmail.com>
2023-07-31 20:56:50 -0300

    Operador de endereço

---
+++ 2023_I/EADII/Aula02/Exemplo01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+  int main() {
+    int a = 6;
+    int b = 8;
+
+    printf("Valor de a= %d", a);
+    printf(" e endereco de a= %d \n", &a);
+
+    printf("Valor de b= %d", b);
+    printf(" e endereco de b= %d", &b);
+    return 0;
+  }
